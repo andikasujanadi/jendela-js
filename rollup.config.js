009f72ce7849bb81ec722b9a6b88ddf3842f2674
Andika Sujanadi <andikasujanadi@gmail.com>
2025-01-19 00:41:20 +0800

    update rollup config

--- rollup.config.js
+++ rollup.config.js
@@ -6,7 +6,10 @@ import postcss from "rollup-plugin-postcss";
 import livereload from "rollup-plugin-livereload";
 import serve from "rollup-plugin-serve";
 
+const isDev = process.env.ROLLUP_WATCH; // Detect if Rollup is in watch mode
+
 export default [
+  // UMD Build (for browser usage)
   {
     input: "src/jendela.js",
     output: {
@@ -16,7 +19,7 @@ export default [
     },
     plugins: [
       postcss({
-        extract: "dist/styles.css", // Specify output CSS file
+        extract: "styles.umd.css", // Separate CSS file for UMD build
         minimize: true, // Minify CSS
       }),
       resolve(),
@@ -24,26 +27,30 @@ export default [
       babel({
         exclude: ["node_modules/**"],
       }),
-      serve({
-        open: true,
-        contentBase: "",
-      }),
-      livereload("dist"),
+      ...(isDev
+        ? [
+            serve({
+              open: true,
+              contentBase: "",
+            }),
+            livereload("dist"),
+          ]
+        : []),
     ],
   },
+
+  // CJS and ESM Builds (for library usage)
   {
     input: "src/jendela.js",
     output: [
-      { file: pkg.main, format: "cjs" },
-      { file: pkg.module, format: "es" },
+      { file: pkg.main, format: "cjs" }, // CommonJS
+      { file: pkg.module, format: "es" }, // ES Module
     ],
     plugins: [
       postcss({
-        extract: "dist/styles.css", // Specify output CSS file
+        extract: "styles.css", // Shared CSS for CJS/ES builds
         minimize: true, // Minify CSS
       }),
-      resolve(),
-      commonjs(),
       babel({
         exclude: ["node_modules/**"],
       }),
